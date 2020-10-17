199efe093590c8ae51d2d9c76ddf1f3c922373d4
lonzthetog <ll.catarino@campus.fct.unl.pt>
2020-10-17 21:44:14 +0100

    refactored packages

--- src/org/academiadecodigo/anderdogs/Main.java
+++
@@ -1,9 +0,0 @@
-package org.academiadecodigo.anderdogs;
-
-import org.academiadecodigo.anderdogs.game.Game;
-
-public class Main {
-    public static void main(String[] args) {
-        Game game = new Game();
-    }
-}
